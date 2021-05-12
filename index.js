d7ccaa4e205cc570804d90f3f421b48d1f67557f
KhaledYousev <mr.khaledyusuf@gmail.com>
2021-05-13 00:48:17 +0300

    Pulse Done

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-const users = ["John", "Mark"];
+app.use(express.json());
+
+let users = ["John", "Mark"];
 
 const logUsers = (req, res, next) => {
   console.log(users);
@@ -16,12 +18,28 @@ const logMethod = (req, res, next) => {
   next();
 };
 
-app.use("/users", logMethod);
-app.get("/users", (req, res, next) => {
-  res.json(users);
-});
-
-
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
+});
+
+/////////////////////////// Pulse Q5//////////////////////////////////////////////
+
+app.get("/users", logMethod, (req, res, next) => {
+  if (users.length === 0) {
+    const err = new Error("No users");
+    err.status = 500;
+    next(err);
+  } else {
+    res.json(users);
+  }
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status);
+  res.json({
+    error: {
+      status: err.status,
+      message: err.message,
+    },
+  });
 });
