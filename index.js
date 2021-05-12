dc0610be61f4941f0a4fb40ba47a848d8d164f8e
KhaledYousev <mr.khaledyusuf@gmail.com>
2021-05-12 23:31:53 +0300

    Pulse 1 , 2 ,3

--- index.js
+++ index.js
@@ -5,11 +5,22 @@ const port = 3000;
 
 const users = ["John", "Mark"];
 
+const logUsers = (req, res, next) => {
+  console.log(users);
+  next();
+};
+app.use(logUsers);
 
+const logMethod = (req, res, next) => {
+  console.log(req.method);
+  next();
+};
 
+app.use("/users", logMethod);
 app.get("/users", (req, res, next) => {
   res.json(users);
 });
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
